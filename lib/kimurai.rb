62c4334e4f313e5f95a85a1807813d05b16f64cd
Victor Afanasev <vicfreefly@gmail.com>
2026-01-13 15:43:42 +0300

    Allow configure Nukitori via Kimurai

--- lib/kimurai.rb
+++ lib/kimurai.rb
@@ -21,6 +21,33 @@ require_relative 'kimurai/pipeline'
 require_relative 'kimurai/base'
 
 module Kimurai
+  # Settings that will be forwarded to Nukitori configuration
+  NUKITORI_SETTINGS = %i[
+    openai_api_key
+    anthropic_api_key
+    gemini_api_key
+    vertexai_project_id
+    vertexai_location
+    deepseek_api_key
+    mistral_api_key
+    perplexity_api_key
+    openrouter_api_key
+    gpustack_api_key
+    openai_api_base
+    gemini_api_base
+    ollama_api_base
+    gpustack_api_base
+    openai_organization_id
+    openai_project_id
+    openai_use_system_role
+    bedrock_api_key
+    bedrock_secret_key
+    bedrock_region
+    bedrock_session_token
+    default_model
+    model_registry_file
+  ].freeze
+
   class << self
     def configuration
       @configuration ||= OpenStruct.new
@@ -28,6 +55,22 @@ module Kimurai
 
     def configure
       yield(configuration)
+      apply_nukitori_configuration
+    end
+
+    def apply_nukitori_configuration
+      nukitori_settings = NUKITORI_SETTINGS.filter_map do |setting|
+        value = configuration[setting]
+        [setting, value] if value
+      end.to_h
+
+      return if nukitori_settings.empty?
+
+      Nukitori.configure do |config|
+        nukitori_settings.each do |setting, value|
+          config.public_send("#{setting}=", value)
+        end
+      end
     end
 
     def env
